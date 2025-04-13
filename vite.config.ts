f2f02d2dc7f52747f2f5715380b3708f7c80f773
DbDibyendu <dbdibyendu5@gmail.com>
2025-04-13 11:08:18 +0530

    fixed repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/teleparty-chat/", // important for GitHub Pages
+  base: "/teleparty-ass/", // important for GitHub Pages
 });
